7d9076dde0bd113b3c548ae0766ad582811c21a5
nigar4 <nigar-djafarova@mail.ru>
2022-10-27 17:34:50 +0400

    Add project files.

---
+++ OOP/Animal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public string Eat { get; set; }
+        public BiomType Biom { get; set; }
+        public int Age { get; set; }
+        public int Weight { get; set; }
+
+        public double Area { get; set; }
+
+        public string[] WhatDoesHeEat { get; set; }
+
+        public bool IsPradator { get; protected set; }
+
+        protected string _sound;
+
+        public void ToEat(string eat, double kg)
+        {
+            if (eat.Contains(eat))
+            {
+                Console.WriteLine($"{Name}ate{kg}kilogram{eat}");
+            }
+        }
+
+        public Animal(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+
+    }
+}
+
+ 
+
---
+++ OOP/Elephant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+ public class Elephant:Animal
+    {
+        public Elephant(string name, int age) : base(name, age)
+        {
+            Biom = BiomType.Tropic;
+            Eat = "trava";
+            _sound = "UUUUU";
+        }
+    }
+}
